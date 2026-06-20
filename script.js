2d84515c1ef791e2393ed570d733c5fb225db14d
safa <safa.gitclass@gmail.com>
2026-06-20 02:40:57 -0700

    Added Calculator Code

---
+++ script.js
@@ -0,0 +1,7 @@
+function add(){
+    let a = Number(document.getElementById("num1").value);
+    let b = Number(document.getElementById("num2").value);
+
+    document.getElementById("result").innerHTML =
+        "Result = " + (a + b);
+}
